96887d675f866c748badf87b3ed30006d0cd0b80
Alina <alinaa2501@gmail.com>
2025-03-28 14:19:17 +0200

    adding starting project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Vlad',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button>Change name</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
